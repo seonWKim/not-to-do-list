59f9723af5f957ebf10e1f06ff9bc8e40032d426
seonwoo960000 <seonwoo960000@naver.com>
2021-10-01 17:18:11 +0900

    publishing on github

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:seonwoo960000/not-to-do-list.git master:gh-pages
+git push -f git@github.com:seonwoo960000/not-to-do-list-site.git master:gh-pages
 cd -
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/not-to-do-list/'
+    ? '/not-to-do-list-site/'
     : '/'
 }
